08cf41bfc6b082a66f962b72ad4e7284eb3e7df2
Alec Swanson <alecswanson@gmail.com>
2024-01-25 23:54:49 -0800

    bootstrap comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -49,6 +49,7 @@ vim.o.tabstop = 2
 -- Always show the auto complete menu
 vim.o.completeopt = "menuone"
 
+-- Bootstrap package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -61,6 +62,7 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
+-- End bootstrap
 
 require("lazy").setup({
 	{
