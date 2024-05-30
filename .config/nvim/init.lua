919272520cd3b9b02ae809ae28604ec56f85ed89
Alec Swanson <aleceswanson@gmail.com>
2024-05-30 15:25:08 -0700

    fix up completion key bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -192,8 +192,7 @@ require("lazy").setup({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<C-CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				})
 			})
 		end
